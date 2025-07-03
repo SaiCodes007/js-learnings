b7e1465f7fc131c6769a4ec33b484655b3ab3d18
Sai <106392933+SaiCodes007@users.noreply.github.com>
2025-07-03 14:31:50 +0000

    Conversion of DataTypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,11 @@
+let score = 45
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let isloggedIn = "sai"
+
+let booleanisloggedIn = Boolean(isloggedIn)
+
+console.log(booleanisloggedIn);
+
