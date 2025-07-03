5a6181af1b48af6564453a73ce2568631b5667ae
Sai <106392933+SaiCodes007@users.noreply.github.com>
2025-07-03 22:04:39 +0000

    functions in JS

---
+++ func.js/01_func.js
@@ -0,0 +1,17 @@
+function sayMyName(){
+    console.log("s");
+    console.log("a");
+    console.log("i");
+    
+}
+// sayMyName()
+
+function addTwoNum(num1 , num2){
+    return num1 + num2
+    // return result
+}
+
+const result = addTwoNum(11,22)
+
+//console.log("Result: ", result);
+
