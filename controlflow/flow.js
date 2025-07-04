cee84bc8abc3c99007fb1b9cc901b00ff50807dd
Sai <106392933+SaiCodes007@users.noreply.github.com>
2025-07-04 16:07:55 +0000

    Switch and Truthy values

---
+++ controlflow/flow.js
@@ -0,0 +1,9 @@
+const balance = 1000
+
+// if (balance > 500) {
+//     console.log("test");
+    
+// }
+
+// if (balance > 500) console.log("test") , console.log("Yessss");
+
---
+++ controlflow/truthy.js
@@ -0,0 +1,7 @@
+const userEmail = "sai@mail.com";
+
+if (userEmail){
+    console.log("Mail recieved" );
+} else {
+    console.log("Don't have user email");
+}
